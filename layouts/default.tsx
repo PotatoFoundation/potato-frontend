480058d644c7af4cd9d7fee35b9a314047e02978
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-10-28 22:09:44 +0200

    Migrate all to Potato

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./Button.module.css";
-import logo from "./../images/DurianLogo.png";
+import logo from "./../images/PotatoLogo.png";
+
+import styles from "./default.module.scss";
 
 export interface DefaultLayoutProps {
   children: React.ReactNode;
@@ -12,14 +13,17 @@ const DefaultLayout: React.FunctionComponent<DefaultLayoutProps> = ({
 }: DefaultLayoutProps) => {
   return (
     <>
-      <header>
-        <Image
-          alt="logo"
-          src={logo}
-					width={100}
-					height={100}
-        />
-        This is header
+      <header className={styles.Header}>
+        <div className={styles.LogoContainer}>
+          <Image
+            alt="logo"
+            className={styles.LogoHeight}
+            src={logo}
+            width={80}
+            height={80}
+          />
+          <span className={styles.LogoTitle}>We are Potato!</span>
+        </div>
       </header>
       <main>{props.children}</main>
       <footer>This is footer</footer>
